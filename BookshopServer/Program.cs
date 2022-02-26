774366335a05bfe0c3a81ff24886277b953cc6a5
Sahan Serasinghe <sahan.serasinghe@gmail.com>
2022-02-26 11:41:23 +1030

    Add additional config

--- BookshopServer/Program.cs
+++ BookshopServer/Program.cs
@@ -1,17 +1,33 @@
 using BookshopServer.Services;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using System.Runtime.InteropServices;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Additional configuration is required to successfully run gRPC on macOS.
 // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+builder.WebHost.ConfigureKestrel(options =>
+{
+  // Setup a HTTP/2 endpoint without TLS.
+  options.ListenLocalhost(5000, o => o.Protocols =
+      HttpProtocols.Http2);
+});
 
 // Add services to the container.
 builder.Services.AddGrpc();
+builder.Services.AddGrpcReflection();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.MapGrpcService<InventoryService>();
+
+// Enable reflection in Debug mode.
+if (app.Environment.IsDevelopment())
+{
+  app.MapGrpcReflectionService();
+}
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
 
 app.Run();
