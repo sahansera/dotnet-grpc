e02d7be44a521d2784424a46b304c86f284a951f
Sahan Serasinghe <sahan.serasinghe@gmail.com>
2022-03-15 08:52:05 +1030

    Add gRPC client

---
+++ BookshopClient/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using Grpc.Net.Client;
+using Bookshop;
+
+// The port number must match the port of the gRPC server.
+using var channel = GrpcChannel.ForAddress("http://localhost:5000");
+var client = new Inventory.InventoryClient(channel);
+var reply = await client.GetBookListAsync(new GetBookListRequest { });
+Console.WriteLine("Books: " + reply.Books);
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
