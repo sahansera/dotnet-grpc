d931d19cbe5a91e12cc8380e0b93f646afe7ce64
Sahan Serasinghe <sahan.serasinghe@gmail.com>
2022-09-03 10:38:13 +0930

    Add deployment scripts

---
+++ BookshopServer/Infrastructure/deploy.sh
@@ -0,0 +1,26 @@
+# Deploy using ARM template
+# Usage: ./deploy.sh <resource group name> <location> <template file> <parameters file>
+# Example: ./deploy.sh BookshopServer westeurope azuredeploy.json azuredeploy.parameters.json
+
+# Check for required parameters
+if [ $# -lt 4 ]; then
+    echo "Usage: ./deploy.sh <resource group name> <location> <template file> <parameters file>"
+    exit 1
+fi
+
+# Set variables
+RESOURCE_GROUP_NAME=$1
+LOCATION=$2
+TEMPLATE_FILE=$3
+PARAMETERS_FILE=$4
+
+# Create resource group
+az group create --name $RESOURCE_GROUP_NAME --location $LOCATION
+
+# Deploy template
+az group deployment create --resource-group $RESOURCE_GROUP_NAME \
+ --template-file $TEMPLATE_FILE \
+ --parameters $PARAMETERS_FILE \
+ --parameters name=$RESOURCE_GROUP_NAME serverFarmResourceGroup=$RESOURCE_GROUP_NAME 
+
+echo "Deployment complete"
